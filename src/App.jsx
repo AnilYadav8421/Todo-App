ee3efdb2c9da1dbecdfc9f77012cfaca02a6a6d7
Anil Yadav <anilyadav8149838058@gmail.com>
2025-04-11 11:07:36 +0530

    Setup React app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>Hello, my name is anil</h1>
+    </div>
+  )
+}
+
+export default App
